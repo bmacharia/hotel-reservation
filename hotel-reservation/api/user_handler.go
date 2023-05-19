672bc0d5dce0f7f66371ce273592659d75e6ef60
Babu Macharia <babu.macharia@gmail.com>
2023-05-19 12:52:41 -0700

    Add database layer

--- hotel-reservation/api/user_handler.go
+++ hotel-reservation/api/user_handler.go
@@ -1,18 +1,42 @@
 package api
 
 import (
+	"context"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/hotel-reservation/hotel-reservation/db"
 	"github.com/hotel-reservation/hotel-reservation/types"
 )
 
-func HandleGetUsers(c *fiber.Ctx) error {
+type UserHandler struct {
+	userStore db.UserStore
+}
+
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+
+}
+
+func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
+	var (
+		id  = c.Params("id")
+		ctx = context.Background()
+	)
+
+	user, err := h.userStore.GetUserByID(ctx, id)
+	if err != nil {
+		return err
+
+	}
+	return c.JSON(user)
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
 	u := types.User{
 		FirstName: "Babu",
 		LastName:  "Doube Fisting",
 	}
 	return c.JSON(u)
-}
-
-func HandleGetUser(c *fiber.Ctx) error {
-	return c.JSON("Babu")
 }
--- hotel-reservation/main.go
+++ hotel-reservation/main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/hotel-reservation/hotel-reservation/api"
+	"github.com/hotel-reservation/hotel-reservation/db"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+const dburi = "mongodb://localhost:27017"
+const dbname = "hotel-reservation"
+const userColl = "users"
+
+// Create a new fiber instance with custom config
+var config = fiber.Config{
+	// Override default error handler
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
+
+// ...
 
 func main() {
 	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
 	flag.Parse()
 
-	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// handler initialization
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+
+	app := fiber.New(config)
 	apiv1 := app.Group("/api/v1")
 
-	apiv1.Get("/user", api.HandleGetUsers)
-	apiv1.Get("/user/:id", api.HandleGetUser)
+	apiv1.Get("/user", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
 	app.Listen(*listenAddr)
 }
